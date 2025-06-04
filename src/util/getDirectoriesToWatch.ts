6a5d952c62f1412112b861be408dad4a8ffed7b5
eXory2024 <exory@anio.software>
2025-06-04 12:09:29 +0200

    Add getDirectoriesToWatch.

---
+++ src/util/getDirectoriesToWatch.ts
@@ -0,0 +1,27 @@
+import type {ProjectContext} from "../ProjectContext.js"
+import path from "node:path"
+
+export function getDirectoriesToWatch(project: ProjectContext): string[] {
+	const {ts, logger} = project.internal
+
+	const config = ts.readConfigFile(
+		project.tsconfigPath,
+		ts.sys.readFile
+	)
+
+	if (config.error) {
+		logger.error(
+			new Error(`Unable to read tsconfig.json file`)
+		)
+
+		return []
+	}
+
+	const parsedConfig = ts.parseJsonConfigFileContent(
+		config.config,
+		ts.sys,
+		path.dirname(project.tsconfigPath)
+	)
+
+	return Object.keys(parsedConfig.wildcardDirectories ?? {})
+}
