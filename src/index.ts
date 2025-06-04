2f1d4fa670a9604caaecf02c7566a0db00f56156
eXory2024 <exory@anio.software>
2025-06-04 11:22:04 +0200

    Initialize project context.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import type tsModule from "typescript/lib/tsserverlibrary"
 import type {Options} from "./Options.js"
 import type {ProjectContext} from "./ProjectContext.js"
 import {createLogger} from "./createLogger.js"
+import {createJobRunner} from "./createJobRunner.js"
 
 const projectContextMap: Map<string, ProjectContext> = new Map()
 
@@ -24,6 +25,22 @@ const init: tsModule.server.PluginModuleFactory = ({typescript: ts}) => {
 
 		logger.log(`compiler options: ${JSON.stringify(compilerOptions)}`)
 		logger.log(`options: ${JSON.stringify(pluginOptions)}`)
+
+		const projectName = info.project.getProjectName()
+
+		//
+		// this portion of the code will only run once per project
+		//
+		if (!projectContextMap.has(projectName)) {
+			logger.log(`initializing context for project '${projectName}'`)
+
+			projectContextMap.set(projectName, {
+				chokidarInstance: undefined as any,
+				jobRunner: createJobRunner(logger)
+			})
+		}
+
+		const projectContext = projectContextMap.get(projectName)!
 
 		return proxy
 	}
