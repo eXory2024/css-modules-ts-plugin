6713646d93f947e2ef2d52b03a8f59b8609afddb
eXory2024 <exory@anio.software>
2025-06-05 17:07:34 +0200

    Resolving of path aliases should now work :).

--- src/resolveModuleSpecifier.ts
+++ src/resolveModuleSpecifier.ts
@@ -43,6 +43,44 @@ export function resolveModuleSpecifier(
 		}
 	}
 
+	if (compilerOptions.paths) {
+		// instead of trying to resolve paths on our own,
+		// we use a synthetic file name instead and tell typescript that this fake file exists
+		// we then use the "fake" resolved path and remove the synthetic file extension
+		// adding .d.css.ts in its place
+		const syntheticFileExtension = `__virtual_${Math.random().toString(32).slice(2)}.ts`
+
+		const result = ts.resolveModuleName(
+			`${extensionlessModuleSpecifier}${syntheticFileExtension}`,
+			containingFile,
+			compilerOptions,
+			{
+				...ts.sys,
+				fileExists(fileName) {
+					return fileName.endsWith(syntheticFileExtension)
+				}
+			}
+		)
+
+		if (result.resolvedModule) {
+			const {resolvedFileName} = result.resolvedModule
+
+			if (resolvedFileName.endsWith(syntheticFileExtension)) {
+				const tmp = resolvedFileName.slice(0, -(syntheticFileExtension.length))
+
+				logger.log(`resolved '${moduleSpecifier}' to '${tmp}.d.css.ts'`)
+
+				return {
+					resolvedModule: {
+						resolvedFileName: `${tmp}.d.css.ts`,
+						extension: ts.Extension.Dts,
+						isExternalLibraryImport: false
+					}
+				}
+			}
+		}
+	}
+
 	return {
 		resolvedModule: undefined
 	}
