855ed9ad469a8b9ce695d6b820d29358ec384760
eXory2024 <exory@anio.software>
2025-06-05 12:19:15 +0200

    Implement onFileAdded event handler.

--- src/events/onFileAdded.ts
+++ src/events/onFileAdded.ts
@@ -5,5 +5,34 @@ export function onFileAdded(
 	project: ProjectContext,
 	pathInfo: PathInfo
 ) {
+	const {ts, info} = project.internal
+
 	project.logger.log(`chokidar:added ${pathInfo.cssSourcePath}`)
+
+	//
+	// create and open the virtual d.css.ts file
+	//
+	const tsScriptInfo = info.project.projectService.getOrCreateScriptInfoForNormalizedPath(
+		pathInfo.normalizedTSServerPath,
+		true,
+		// NB: needs to be a module
+		// todo: init with class names from css file
+		`declare const _defaultExport: {};\nexport = _defaultExport;\n`,
+		ts.ScriptKind.TS,
+		false
+	)
+
+	if (!tsScriptInfo) {
+		project.logger.log(`warning tsScriptInfo is undefined!`)
+
+		return
+	}
+
+	project.logger.log(`adding to virtual files`)
+
+	project.virtualFiles.set(pathInfo.normalizedTSServerPath, {
+		sourceFilePath: pathInfo.cssSourcePath,
+		normalizedTSServerPath: pathInfo.normalizedTSServerPath,
+		tsScriptInfo
+	})
 }
