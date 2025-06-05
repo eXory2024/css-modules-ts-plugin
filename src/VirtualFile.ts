2f7618af7dc6bf377a1c2604f28d5733202f4490
eXory2024 <exory@anio.software>
2025-06-05 11:38:41 +0200

    Add comments.

--- src/VirtualFile.ts
+++ src/VirtualFile.ts
@@ -1,6 +1,13 @@
 import type tsModule from "typescript/lib/tsserverlibrary"
 
 export type VirtualFile = {
+	//
+	// path of the virtual .d.css.ts file
+	//
 	normalizedTSServerPath: tsModule.server.NormalizedPath
+
+	//
+	// path to the .css source file represented by this virtual file
+	//
 	sourceFilePath: string
 }
