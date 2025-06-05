00bd9905de2d6ffa05a5fb0f6666be7872e448f4
eXory2024 <exory@anio.software>
2025-06-05 15:22:56 +0200

    Update virtual .d.css.ts file content when .css file on disk has changed.

--- src/events/onFileChanged.ts
+++ src/events/onFileChanged.ts
@@ -1,9 +1,25 @@
 import type {ProjectContext} from "../ProjectContext.js"
 import type {PathInfo} from "../setupChokidarInstance.js"
+import {generateCSSDeclarationCodeForFile} from "../util/css/generateCSSDeclarationCodeForFile.js"
 
 export function onFileChanged(
 	project: ProjectContext,
 	pathInfo: PathInfo
 ) {
 	project.logger.log(`chokidar:changed ${pathInfo.cssSourcePath}`)
+
+	if (project.virtualFiles.has(pathInfo.normalizedTSServerPath)) {
+		project.logger.log(`updating virtual file`)
+
+		const vFile = project.virtualFiles.get(pathInfo.normalizedTSServerPath)!
+
+		vFile.tsScriptInfo.editContent(
+			0,
+			vFile.tsScriptInfo.getSnapshot().getLength(),
+			generateCSSDeclarationCodeForFile(pathInfo.cssSourcePath)
+		)
+
+		vFile.tsScriptInfo.markContainingProjectsAsDirty()
+		project.internal.info.project.refreshDiagnostics()
+	}
 }
