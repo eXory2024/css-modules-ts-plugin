4d463038b3b7d34ad0968602766144310fd2958c
eXory2024 <exory@anio.software>
2025-06-05 11:53:32 +0200

    Do some initial logging in resetAndSetupProject.

--- src/resetAndSetupProject.ts
+++ src/resetAndSetupProject.ts
@@ -3,5 +3,9 @@ import type {ProjectContext} from "./ProjectContext.js"
 export async function resetAndSetupProject(
 	project: ProjectContext
 ): Promise<undefined> {
-	
+	project.logger.log(`resetAndSetupProject called`)
+
+	if (project.chokidarInstance !== undefined) {
+		project.logger.log(`chokidar instance needs to be destroyed`)
+	}
 }
