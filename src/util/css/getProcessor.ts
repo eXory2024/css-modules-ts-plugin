2e45757c7f2fd640afdda02448587dd24c9fd0a8
eXory2024 <exory@anio.software>
2025-06-05 16:33:12 +0200

    Add source comment.

--- src/util/css/getProcessor.ts
+++ src/util/css/getProcessor.ts
@@ -1,3 +1,6 @@
+//
+// source: https://github.com/mrmckeb/typescript-plugin-css-modules/blob/045fc08805a6e56663fc8a42225af46ff48f6e1e/src/helpers/getProcessor.ts
+//
 import postcss, {type AcceptedPlugin} from "postcss"
 import Processor from "postcss/lib/processor"
 import postcssLocalByDefault from "postcss-modules-local-by-default"
