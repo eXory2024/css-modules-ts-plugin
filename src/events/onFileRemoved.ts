56fd73d86a0dcd9111f356e0fdcfe73ff6fff52b
eXory2024 <exory@anio.software>
2025-06-05 15:13:39 +0200

    Use the same logic as in resetAndSetupProject when a CSS file gets deleted.

--- src/events/onFileRemoved.ts
+++ src/events/onFileRemoved.ts
@@ -6,4 +6,19 @@ export function onFileRemoved(
 	pathInfo: PathInfo
 ) {
 	project.logger.log(`chokidar:removed ${pathInfo.cssSourcePath}`)
+
+	if (project.virtualFiles.has(pathInfo.normalizedTSServerPath)) {
+		project.logger.log(`removing single virtual file`)
+
+		const vFile = project.virtualFiles.get(
+			pathInfo.normalizedTSServerPath
+		)!
+
+		// not sure what the correct order is here ...
+		// or if .closeClientFile() would be enough
+		vFile.tsScriptInfo.close()
+		vFile.tsScriptInfo.detachAllProjects()
+
+		project.virtualFiles.delete(pathInfo.normalizedTSServerPath)
+	}
 }
